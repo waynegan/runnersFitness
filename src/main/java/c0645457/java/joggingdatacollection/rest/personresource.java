0403dab3fbc1aab9520b2214801d4e738fdd6771
waynegan <ganwei92@gmail.com>
2015-08-09 22:33:11 -0400

    implement database SQL and restful function

--- src/main/java/c0645457/java/joggingdatacollection/rest/personresource.java
+++ src/main/java/c0645457/java/joggingdatacollection/rest/personresource.java
@@ -6,19 +6,27 @@
 
 package c0645457.java.joggingdatacollection.rest;
 
+import c0645457.java.joggingdatacollection.entities.Person;
+import c0645457.java.joggingdatacollection.entities.personListWrapper;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * REST Web Service
@@ -34,30 +42,78 @@ public class personresource {
  @PersistenceContext
    private EntityManager entityManager;
 
-    /**
-     * Creates a new instance of personresource
-     */
-    public personresource() {
+     private Integer countPersons() {
+        Query query = entityManager.createQuery("SELECT COUNT(p.id) FROM Person p");
+        return ((Long) query.getSingleResult()).intValue();
     }
-
+     @SuppressWarnings("unchecked")
+    private List<Person> findPersons(int startPosition, int maxResults, String sortFields, String sortDirections) {
+        Query query =
+                entityManager.createQuery("SELECT p FROM Person p ORDER BY p." + sortFields + " " + sortDirections);
+        query.setFirstResult(startPosition);
+        query.setMaxResults(maxResults);
+        return query.getResultList();
+    }
+     private personListWrapper findPersons(personListWrapper wrapper) {
+        wrapper.setTotalResults(countPersons());
+        int start = (wrapper.getCurrentPage() - 1) * wrapper.getPageSize();
+        wrapper.setList(findPersons(start,
+                                    wrapper.getPageSize(),
+                                    wrapper.getSortFields(),
+                                    wrapper.getSortDirections()));
+        return wrapper;
+    }
     /**
      * Retrieves representation of an instance of c0645457.java.joggingdatacollection.rest.personresource
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces("application/json")
-    public String getJson() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    @Produces(MediaType.APPLICATION_JSON)
+    public personListWrapper listPersons(@DefaultValue("1")
+                                            @QueryParam("page")
+                                            Integer page,
+                                            @DefaultValue("id")
+                                            @QueryParam("sortFields")
+                                            String sortFields,
+                                            @DefaultValue("asc")
+                                            @QueryParam("sortDirections")
+                                            String sortDirections) {
+        personListWrapper personListWrapper = new personListWrapper();
+        personListWrapper.setCurrentPage(page);
+        personListWrapper.setSortFields(sortFields);
+        personListWrapper.setSortDirections(sortDirections);
+        personListWrapper.setPageSize(10);
+        return findPersons(personListWrapper);
     }
 
-    /**
-     * PUT method for updating or creating an instance of personresource
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-    @PUT
-    @Consumes("application/json")
-    public void putJson(String content) {
+    @GET
+    @Path("{id}")
+    public Person getPerson(@PathParam("id") Long id) {
+        return entityManager.find(Person.class, id);
+    }
+
+    @POST
+    public Person savePerson(Person person) {
+        if (person.getId() == null) {
+            Person personToSave = new Person();
+            personToSave.setName(person.getName());
+            personToSave.setDescription(person.getDescription());
+            personToSave.setImageUrl(person.getImageUrl());
+            entityManager.persist(person);
+        } else {
+            Person personToUpdate = getPerson(person.getId());
+            personToUpdate.setName(person.getName());
+            personToUpdate.setDescription(person.getDescription());
+            personToUpdate.setImageUrl(person.getImageUrl());
+            person = entityManager.merge(personToUpdate);
+        }
+
+        return person;
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deletePerson(@PathParam("id") Long id) {
+        entityManager.remove(getPerson(id));
     }
 }
